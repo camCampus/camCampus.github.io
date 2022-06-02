01ee33bd98a4b30bd670db314022f5832e3fb2bd
camCampus <101818645+camCampus@users.noreply.github.com>
2022-06-02 13:49:09 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+const body = document.body;
+var cursor = document.querySelector(".cursor");
+var cursor2 = document.querySelector(".cursor2");
+
+document.addEventListener("mousemove", function(e){
+    cursor.style.cssText = cursor2.style.cssText = "left:" + e.clientX + "px; top: " + e.clientY + "px;";
+    console.log("ok");
+})
+setInterval(rainFall, 10);
+
+function rainFall(){
+    const waterDrop = document.createElement('i');
+
+    waterDrop.classList.add("fa-solid");
+    waterDrop.classList.add("fa-droplet");
+    waterDrop.style.fontSize = Math.random() * 7 + 'px';
+    waterDrop.style.animationDuration = Math.random() * 2 + 's';
+    waterDrop.style.opacity = Math.random() + 0.5;
+    waterDrop.style.left = Math.random() * window.innerWidth + 'px';
+    
+    body.appendChild(waterDrop);
+
+    setTimeout(() => {
+        waterDrop.remove()
+    }, 6000)
+}
