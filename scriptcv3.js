d5ac0bea6c96274f74d70dbfffca8df9298d7962
camCampus <camille.bochar@le-campus-numerique.fr>
2022-06-09 13:38:38 +0200

    test marion line

--- scriptcv3.js
+++ scriptcv3.js
@@ -1,7 +1,40 @@
+gsap.registerPlugin(ScrollTrigger);
+
+
 var cursor = document.querySelector(".cursor");
 var cursor2 = document.querySelector(".cursor2");
 
 document.addEventListener("mousemove", function(e){
     cursor.style.cssText = cursor2.style.cssText = "left:" + e.clientX + "px; top: " + e.clientY + "px;";
 
+})
+/*
+const tl = gsap.timeline({duration:3});
+tl.from(".two", {xPercent:-100})
+.from(".trois", {xPercent:-100})
+.from(".quatre", {xPercent:-100});
+
+ScrollTrigger.creat({
+    animation:tl,
+    trigger:'.first',
+    start: "top top",
+    end:"+=4000",
+    scrub:true,
+    pin:true,
+    anticipatePin:1
+});
+*/
+
+let path = document.querySelector('path');
+let pathLength = path.getTotalLength();
+
+path.style.strokeDasharray = pathLength + ' ' + pathLength;
+path.style.strokeDashoffset = pathLength;
+
+window.addEventListener('scroll', () => {
+    var scrollPercentage = (document.documentElement.scrollTop + document.body.scrollTop) / (document.documentElement.scrollHeight - document.documentElement.clientHeight);
+
+    var drawLength = pathLength * scrollPercentage;
+
+    path.style.strokeDashoffset = pathLength - drawLength;
 })
